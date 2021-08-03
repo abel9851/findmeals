e30a3084ae3ae161b3270122578c8126f79238ad
Heejun Shin <abel9851@gmail.com>
2021-08-03 13:44:23 +0900

    add food admin

--- foods/admin.py
+++ foods/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Food)
+class FoodAdmin(admin.ModelAdmin):
+
+    """Food Admin Definition"""
+
+    pass
