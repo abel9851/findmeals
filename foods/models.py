441b5ef32d7ec65e7ea9ab7533dd94218a734dac
Heejun Shin <abel9851@gmail.com>
2021-08-03 13:32:52 +0900

    fix food model

--- foods/models.py
+++ foods/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from core import models as core_models
 
 
-class Food(models.Model):
+class Food(core_models.TimeStampedModel):
     """Food Model Definition"""
 
     name = models.CharField(max_length=50)
