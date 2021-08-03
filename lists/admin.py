33740ed8d4109d9a56959bf81e6d020187f49e51
Heejun Shin <abel9851@gmail.com>
2021-08-03 13:42:50 +0900

    add list model

--- lists/admin.py
+++ lists/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.list)
+class ListAdmin(admin.ModelAdmin):
+
+    """List Admin Definition"""
+
+    pass
--- lists/models.py
+++ lists/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from core import models as core_models
 
-# Create your models here.
+
+class list(core_models.TimeStampedModel):
+
+    """List Model Definition"""
+
+    name = models.CharField(max_length=50)
+    user = models.ForeignKey(
+        "users.User", related_name="lists", on_delete=models.CASCADE
+    )
+    foods = models.ManyToManyField("foods.Food", related_name="lists", blank=True)
+
+    def __str_(self):
+        return self.name
