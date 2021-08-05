1d2accbda58577d8fa9d67a144863c730d8be803
Heejun Shin <abel9851@gmail.com>
2021-08-05 14:41:26 +0900

    change upload dir on ImageField(avatar)

--- users/models.py
+++ users/models.py
@@ -5,6 +5,5 @@ from django.db import models
 class User(AbstractUser):
     """Custom User Model"""
 
-    avatar = models.ImageField(blank=True)
+    avatar = models.ImageField(upload_to="avatars", blank=True)
     bio = models.TextField(default="", blank=True)
-
